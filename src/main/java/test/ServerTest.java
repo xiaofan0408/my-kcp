a4f60c954087de292fb9e64ceee1a58bbaad64dc
xiaofan0408 <1002758393@qq.com>
2022-12-09 14:58:42 +0800

    修改测试代码

--- src/main/java/test/ServerTest.java
+++ src/main/java/test/ServerTest.java
@@ -1,19 +1,12 @@
 package test;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.socket.DatagramPacket;
-import io.netty.util.CharsetUtil;
-import net.base.KcpTransportConnection;
-import net.server.KcpServerSession;
+
 import net.server.KcpTransportServer;
-import reactor.core.publisher.Mono;
-import reactor.netty.Connection;
-import reactor.netty.udp.UdpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 import java.nio.charset.StandardCharsets;
-import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 
 /**
@@ -22,48 +15,58 @@ import java.util.concurrent.CountDownLatch;
  */
 public class ServerTest {
 
+    static final Logger logger = LoggerFactory.getLogger(ServerTest.class);
+
+
     public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        KcpTransportServer.create("127.0.0.1",18884)
+                .log(true)
+                .exception(throwable -> System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&"+throwable))
+                .onConnected(connection -> {
+                    System.out.println("有连接进来" + Thread.currentThread().getName() + connection.getLongAdder());
+                })
+                .onReceive((kcpTransportMessage, connection) -> {
+                    System.out.println("server access message: " + new String(kcpTransportMessage.getMessage()));
+                    connection.write("client hello".getBytes(StandardCharsets.UTF_8));
+                })
+                .onClose(connection -> {
+                    System.out.println("有连接断开" + Thread.currentThread().getName() + connection.getLongAdder());
+                })
+                .start()
+                .doOnNext(kcpServerSession -> System.out.println("Server started on the address : "))
+                .block()
+                .bind()
+                .onDispose().block();
+        System.out.println(1111);
+
+        latch.await();
+
 //        CountDownLatch latch = new CountDownLatch(1);
-//        KcpServerSession serverSession= KcpTransportServer.create("127.0.0.1",18884)
-//                .log(true)
-//                .exception(throwable -> System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&"+throwable))
-//                .onConnected(connection -> {
+//        KcpServerConfig kcpServerConfig = new KcpServerConfig();
+//        kcpServerConfig.setOnConnected(connection -> {
 //                    System.out.println("有连接进来" + Thread.currentThread().getName() + connection.getLongAdder());
-//                })
-//                .onReceive((kcpTransportMessage, connection) -> {
+//                });
+//        kcpServerConfig.setOnReceive((kcpTransportMessage, connection) -> {
 //                    System.out.println("server access message: " + new String(kcpTransportMessage.getMessage()));
 //                    connection.write("client hello".getBytes(StandardCharsets.UTF_8));
-//                })
-//                .onClose(connection -> {
+//                });
+//        kcpServerConfig.setOnClose(connection -> {
 //                    System.out.println("有连接断开" + Thread.currentThread().getName() + connection.getLongAdder());
-//                })
-//                .start()
+//                });
+//
+//
+//        KcpServerConn udpServerTransport = UdpServer.create()
+//                .host("127.0.0.1")
+//                .port(18884)
+//                .bind()
+//                .map(KcpTransportConnection::new)
+//                .map(c -> new KcpServerConn(kcpServerConfig,c))
 //                .block();
-//        serverSession
-//                .onDispose()
-//                .block();
+//
+//        System.out.println(11111);
+//        udpServerTransport.bind().onDispose();
 //        latch.await();
-        Connection server =UdpServer.create()
-                .host("127.0.0.1")
-                .port(18884)
-                .handle((in, out) ->
-                        out.sendObject(
-                                in.receiveObject()
-                                        .map(o -> {
-                                            if (o instanceof DatagramPacket) {
-                                                DatagramPacket p = (DatagramPacket) o;
-                                                ByteBuf buf = Unpooled.copiedBuffer("hello", CharsetUtil.UTF_8);
-                                                return new DatagramPacket(buf, p.sender());
-                                            }
-                                            else {
-                                                return Mono.error(new Exception("Unexpected type of the message: " + o));
-                                            }
-                                        })))
-                .bindNow(Duration.ofSeconds(30));
-
-        server.onDispose()
-                .block();
-
     }
 
 }
